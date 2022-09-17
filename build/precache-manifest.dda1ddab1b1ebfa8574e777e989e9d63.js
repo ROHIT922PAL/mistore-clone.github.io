3c6c7514c9af7036b0126608a071eab48709d83c
ROHIT922PAL <107602326+ROHIT922PAL@users.noreply.github.com>
2022-09-17 19:18:11 +0530

    Add files via upload

---
+++ build/precache-manifest.dda1ddab1b1ebfa8574e777e989e9d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "653ad9b3702acef17cb7",
+    "url": "/static/css/main.19cd9634.chunk.css"
+  },
+  {
+    "revision": "653ad9b3702acef17cb7",
+    "url": "/static/js/main.653ad9b3.chunk.js"
+  },
+  {
+    "revision": "f416c42cde0d22c6c73b",
+    "url": "/static/css/1.3368f0a0.chunk.css"
+  },
+  {
+    "revision": "f416c42cde0d22c6c73b",
+    "url": "/static/js/1.f416c42c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66ecd118b24aa31443a74c1908351349",
+    "url": "/index.html"
+  }
+];
